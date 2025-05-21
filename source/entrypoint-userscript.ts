b158456afb3572db449c7f8977c66c1537620d07
Oliver Salzburg <oliver.salzburg@gmail.com>
2025-05-21 16:43:42 +0200

    fix: Userscript doesn't load

--- source/entrypoint-userscript.ts
+++ source/entrypoint-userscript.ts
@@ -2,10 +2,16 @@ import { UserScriptLoader } from "@kitten-science/kitten-scientists/UserScriptLo
 import { redirectErrorsToConsole } from "@oliversalzburg/js-utils/errors/console.js";
 import { KittenAnalysts } from "./KittenAnalysts.js";
 
-(async () => {
+export const main = async () => {
   const userScript = await new UserScriptLoader().waitForGame(KittenAnalysts, "ka");
 
-  window.kittenAnalysts = userScript;
+  UserScriptLoader.window.kittenAnalysts = userScript;
 
   userScript.run();
-})().catch(redirectErrorsToConsole(console));
+};
+
+// We auto-ignite the loader, unless we're running in GreaseMonkey (content script).
+// The content script loader will handle the orchestration of that scenario.
+if (typeof GM === "undefined" || GM?.info?.scriptHandler === "Tampermonkey") {
+  main().catch(redirectErrorsToConsole(console));
+}
